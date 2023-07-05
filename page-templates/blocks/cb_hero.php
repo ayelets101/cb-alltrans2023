4699dd6030b4230b8753327694a473c60b8f694c
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-07-05 10:40:48 +0100

    modified hero title

--- page-templates/blocks/cb_hero.php
+++ page-templates/blocks/cb_hero.php
@@ -7,8 +7,17 @@ $class = $block['className'] ?? null ?: '';
 ?>
 <link rel="preload" as="image" href="<?=$img?>">
 <header class="hero <?=$class?>">
-    <div class="container-xl" style="background-image: url(<?=$img?>)">
+    <div class="container-xl px-0" style="background-image: url(<?=$img?>)">
         <div class="overlay"></div>
-        <h1><?=get_field('title')?></h1>
+        <div class="titles">
+            <h1><?=get_field('title')?></h1>
+        <?php
+        if (get_field('subtitle')) {
+            ?>
+            <h2><?=get_field('subtitle')?></h2>
+            <?php
+        }
+        ?>
+        </div>
     </div>
 </header>
