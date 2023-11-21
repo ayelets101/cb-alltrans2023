7c39f856a4d7e145f38f17b9df9c786bd42ecfa4
ayelets101 <150701171+ayelets101@users.noreply.github.com>
2023-11-21 11:40:17 +0000

    .

--- page-templates/blocks/cb_test_block.php
+++ page-templates/blocks/cb_test_block.php
@@ -1,22 +1,22 @@
-<?php
-$r = new WP_Query(array(
-    'post_type' => 'post',
-    'posts_per_page' => 4,
-));
-if ($r->have_posts())
-    ?>
-<section class="related pb-5 test_block">
-    <div class="container-xl">
-        <h2>Test block</h2>
-        <div class="row g-4">
-                <div class="col-md-6 col-xl-3">
-                    <a class="blog_card" href="<?=get_the_permalink()?>">
-                        <img src="<?=get_the_post_thumbnail_url(get_the_ID(),'large')?>" alt="" class="blog_card__image">
-                        <div class="blog_card__content">
-                            <h3 class="blog_card__title"><?=get_the_title()?></h3>
-                        </div>
-                    </a>
+<section class="test_block">
+    <div class="container-xl py-5">
+        <h2 class="text-center mb-5"><?=get_field('title')?></h2>
+        <div class="row g-4 justify-content-center">
+            <?php
+            while(have_rows('test')) {
+                the_row();
+                ?>
+            <div class="col-sm-2 col-md-4 col-lg-3">
+                <div class="test_cols__card">
+                    <div class="test_cols__img">
+                        <img src="<?=wp_get_attachment_image_url(get_sub_field('photo'),'full')?>">
+                    </div>
+                    <div class="test_cols__name"><?=get_sub_field('name')?></div>
                 </div>
+            </div>
+                <?php
+            }
+            ?>
         </div>
     </div>
 </section>
